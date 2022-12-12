3e9d4be3b4cc2c492e7ad301ae4d471475d685c3
Qingxiang (Allen) Guo <johnleberand@hotmail.com>
2022-12-11 21:12:17 -0600

    Add files via upload

---
+++ content/Chapter1_深度学习微积分基础/4_常见求导公式与法则.py
@@ -0,0 +1,24 @@
+# _*_ coding=utf-8 _*_
+"""
+求导公式
+1. (c)' = 0
+2. (x^a)' = ax^(a-1)
+3. (sinx)' = cosx
+4. (cosx)' = -sinx
+5. (tanx)' = (secx)^2
+6. (cotx)' = -(cscx)^2
+7. (secx)' = secx * tanx
+8. (cscx)' = -(cscx) * (cotx)
+9. (a^x)' = (a^x) * lna
+10. (e^x)' = e^x
+11. (log(a)(x))' = 1/(x*lna)
+12. (lnx)' = 1/x
+
+求导法则
+1. (u+v)' = u' + v'
+2. (u-v)' = u' - v'
+3. (u*v)' = u*v' + v*u'
+4. (c*u)' = c(u)'
+5. (u/v)' = (u'*v - v'*u)/(v^2)
+6.
+"""
