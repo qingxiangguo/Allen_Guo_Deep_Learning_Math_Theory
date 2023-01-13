9bb74cdb1482029bbe76f7ca31ae65bd03986db4
qingxiangguo <johnleberand@hotmail.com>
2023-01-13 13:02:45 -0600

    normal

---
+++ content/Chapter2_泰勒展开_梯度下降原理/3_梯度下降算法的直觉理解.py
@@ -0,0 +1,3 @@
+https://www.bilibili.com/video/BV1oY411N7Xz/?spm_id_from=333.337.search-card.all.click&vd_source=9f33dd7a5eac6a4ec0b3028a044ffe9d
+表示每个训练数据点 (xi,yi)(xi,yi)到拟合直线 yˆi=θ0+θ1∙xiy^i=θ0+θ1∙xi的竖直距离的平方和，
+通过最小化这个损失函数来求得拟合直线的最佳参数 θθ，实际上就是求损失函数C在取得最小值情况下 θθ的值。
